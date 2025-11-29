aeb6f121e56c9a90b7887397bb1cad5649240c7e
Atharva Singh <atharvasingh0072@gmail.com>
2025-11-29 22:07:29 +0530

    mongodb update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-import express from "express";
 import dotenv from 'dotenv'
+dotenv.config();
+import express from "express";
 import path from 'path'
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import { checkForAuthenticationsCookie } from "./middleware/authentication.js";
 import userRoute from './routes/router-user.js'
 import blogRouter from './routes/router-blog.js'
+import methodOverride from "method-override";
 
 
 import Blog from "./models/models-blog.js";
-import User from "./models/models-user.js";
 const app =  express();
-dotenv.config();
-const PORT = process.env.PORT || 4001;
+
+const PORT = process.env.PORT ;
 mongoose.connect(process.env.MONGODB_URL)
 .then((e)=> console.log("mongoDB is connected"))
 .catch((e)=>console.log("mongoDB is not connected"));
 app.set('view engine','ejs')
 app.set('views',path.resolve('./views'))
-
+app.use(methodOverride("_method"));
 
 
 app.use(express.json());
